18fb7ff35f82a7b8b4dff20449d82a67bfe14b99
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-05-07 04:58:08 +0700

    install fluture, fluture express, and babel cjs export plugin

---
+++ src/actions/welcome.js
@@ -0,0 +1,7 @@
+import { Json } from "fluture-express";
+import Future from "fluture";
+
+export default (_) => (_) => {
+  const welcome = "stranger";
+  return Future.resolve(Json({ welcome }));
+};
--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import { dispatcher } from "fluture-express";
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
 
 const app = express();
+
+const dispatch = dispatcher(path.resolve(__dirname, "actions"));
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -16,5 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.get("/json", dispatch("welcome"));
 
 export default app;
