10b7605b3e05efa11a0af2d4f938081e5ae5c951
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-06-12 17:32:11 +0700

    ugly echo

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
-import express from "express";
-import path from "path";
+require("dotenv").config();
+import axios from "axios";
 import cookieParser from "cookie-parser";
-import logger from "morgan";
+import express from "express";
 import { dispatcher } from "fluture-express";
-
+import logger from "morgan";
+import path from "path";
+import Future, { fork } from "fluture";
+import { fromMaybe, gets, is, isNothing, pipe, props } from "sanctuary";
+import $ from "sanctuary-def";
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
 
 const app = express();
+
+const { TOKEN } = process.env;
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
 
 const dispatch = dispatcher(path.resolve(__dirname, "actions"));
 
@@ -20,5 +28,31 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.get("/json", dispatch("welcome"));
+// app.post(URI, dispatch("echo"));
+app.post(URI, async (req, res) => {
+  const getChatId = gets(is($.Integer))(["body", "message", "chat", "id"]);
+  const getText = gets(is($.String))(["body", "message", "text"]);
+  const chatId = getChatId(req);
+  const text = getText(req);
+
+  if (isNothing(chatId) && isNothing(text)) {
+    return res.send();
+  }
+
+  const sendMessage = Future((rej, res) => {
+    axios.post(`${TELEGRAM_API}/sendMessage`, {
+      chat_id: fromMaybe(0)(chatId),
+      text: fromMaybe("")(text),
+    });
+    return () => {};
+  });
+
+  const execute = fork((rej) => console.log("reject", rej))((res) =>
+    console.log("resolve", res)
+  );
+  sendMessage.pipe(execute);
+
+  return res.send();
+});
 
 export default app;
