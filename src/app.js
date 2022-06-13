824be483e65c7c46ba99e3d7c5624256df50082e
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-06-13 20:13:44 +0700

    still ugly but using maybe

--- src/app.js
+++ src/app.js
@@ -2,14 +2,20 @@ require("dotenv").config();
 import axios from "axios";
 import cookieParser from "cookie-parser";
 import express from "express";
+import Future, { fork, reject } from "fluture";
 import { dispatcher } from "fluture-express";
+import { env as flutureEnv } from "fluture-sanctuary-types";
 import logger from "morgan";
 import path from "path";
-import Future, { fork } from "fluture";
-import { fromMaybe, gets, is, isNothing, pipe, props } from "sanctuary";
+import sanctuary from "sanctuary";
 import $ from "sanctuary-def";
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
+
+const S = sanctuary.create({
+  checkTypes: true,
+  env: sanctuary.env.concat(flutureEnv),
+});
 
 const app = express();
 
@@ -30,27 +36,34 @@ app.use("/users", usersRouter);
 app.get("/json", dispatch("welcome"));
 // app.post(URI, dispatch("echo"));
 app.post(URI, async (req, res) => {
-  const getChatId = gets(is($.Integer))(["body", "message", "chat", "id"]);
-  const getText = gets(is($.String))(["body", "message", "text"]);
-  const chatId = getChatId(req);
-  const text = getText(req);
+  const getChatId = S.gets(S.is($.Integer))(["body", "message", "chat", "id"]);
+  const getText = S.gets(S.is($.String))(["body", "message", "text"]);
 
-  if (isNothing(chatId) && isNothing(text)) {
+  const chatId_ = getChatId(req);
+  const text_ = getText(req);
+  if (S.isNothing(chatId_) && S.isNothing(text_)) {
     return res.send();
   }
 
-  const sendMessage = Future((rej, res) => {
-    axios.post(`${TELEGRAM_API}/sendMessage`, {
-      chat_id: fromMaybe(0)(chatId),
-      text: fromMaybe("")(text),
+  const echo = (chatId) => (text) =>
+    Future((rej, res) => {
+      axios.post(`${TELEGRAM_API}/sendMessage`, {
+        chat_id: chatId,
+        text: text,
+      });
+      return () => {};
     });
-    return () => {};
-  });
 
   const execute = fork((rej) => console.log("reject", rej))((res) =>
     console.log("resolve", res)
   );
-  sendMessage.pipe(execute);
+
+  const sendMessage = S.lift2(echo)(chatId_)(text_)
+
+  S.pipe([
+    S.fromMaybe(reject("It broke!")),
+    execute,
+  ])(sendMessage);
 
   return res.send();
 });
