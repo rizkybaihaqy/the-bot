6dee22eb86e4094a97de553dad8bac0138788d53
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-08-19 10:41:31 +0700

    add getChatIdFromRequest()

--- src/lib/telegram/getter.js
+++ src/lib/telegram/getter.js
@@ -5,3 +5,8 @@ export const getMessageFromRequest = S.pipe([
   S.gets(S.is($.Object))(["body", "message"]),
   S.maybeToEither("No Message Found. Did not support updated message"),
 ]);
+
+export const getChatIdFromRequest = S.pipe([
+  S.gets(S.is($.Number))(["body", "message", "chat", "id"]),
+  S.maybeToEither("No Chat Id Found. Who are you?"),
+]);
