9ab9c0b57c0e9cca39240fdb5a56dfe455d43738
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-08-30 16:33:15 +0700

    make echo cmd works

--- src/lib/telegram/command/echo.js
+++ src/lib/telegram/command/echo.js
@@ -1,8 +1,28 @@
 import {Json} from 'fluture-express'
+import {eitherToFuture} from '../../fluture'
 import {S} from '../../sanctuary/instance'
-import {reply} from '../request'
+import {
+  getEntityLength,
+  getEntityOffset,
+  getTextFromRequest,
+} from '../getter'
+import {replyTo} from '../request'
 
-export const echo = S.pipe ([
-  reply ('echo'),
-  S.map ((msg) => Json (msg.data)),
-])
+export const echo = (req) =>
+  S.pipe ([
+    getTextFromRequest,
+    S.map ((txt) =>
+      txt.slice (
+        getEntityOffset (req) + getEntityLength (req),
+      ),
+    ),
+    S.map (S.trim),
+    S.chain (
+      S.ifElse ((txt) => txt === '') ((_) =>
+        S.Left ('Cannot Echo Something That Does Not Exist'),
+      ) (S.Right),
+    ),
+    eitherToFuture,
+    S.chain (replyTo (req)),
+    S.map ((msg) => Json (msg.data)),
+  ]) (req)
