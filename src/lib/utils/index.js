3468119cee8da995f6c2323374f1dd1a3c89c7b8
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-08-30 16:32:18 +0700

    make more variation of tap

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,1 +1,18 @@
+import util from 'util'
+
 export const tap = (x) => (console.log (x), x)
+
+export const tapToString = (x) => (
+  console.log (x.toString ()), x
+)
+
+export const tapDetail = (x) => (
+  console.log (
+    util.inspect (x, {
+      showHidden: false,
+      depth: null,
+      colors: true,
+    }),
+  ),
+  x
+)
