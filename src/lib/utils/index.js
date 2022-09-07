7b4eedea9c8cf964036ae4f003a1c9eef3c7a6eb
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-09-07 17:25:45 +0700

    add tap debug

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,4 +1,5 @@
 import util from 'util'
+const debug = require ('debug') ('the-bot:server')
 
 export const tap = (x) => (console.log (x), x)
 
@@ -17,9 +18,12 @@ export const tapDetail = (x) => (
   x
 )
 
-export const tapJSONStringify = (x) =>
-  (console.log (JSON.stringify (x, null, 2)), x)
+export const tapJSONStringify = (x) => (
+  console.log (JSON.stringify (x, null, 2)), x
+)
 
 export const checkPoint = (txt) => (x) => (
   console.log (txt), x
 )
+
+export const tapDebug = (txt) => (x) => (debug (txt + x), x)
