b3212c81a1bc28c5a7e966c14ef448d1a80b77f4
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-09-09 17:43:39 +0700

    refactor command action by removing ifelse

--- src/actions/command.js
+++ src/actions/command.js
@@ -1,15 +1,14 @@
 import F from 'fluture'
 import {Next} from 'fluture-express'
+import {eitherToFuture} from '../lib/fluture'
 import {S} from '../lib/sanctuary/instance'
 import {getBotCommandFromRequest} from '../lib/telegram/getter'
-import {isBotCommand} from '../lib/telegram/predicate'
 import {command as commandRouting} from '../lib/telegram/routing'
 
 export default (locals) => (req) =>
-  S.ifElse (isBotCommand) (
-    S.pipe ([
-      getBotCommandFromRequest,
-      S.fromRight (''),
-      commandRouting (req),
-    ]),
-  ) ((_) => F.resolve (Next (locals))) (req)
+  S.pipe ([
+    getBotCommandFromRequest,
+    eitherToFuture,
+    S.chain (commandRouting (req)),
+    F.chainRej (S.K (F.resolve (Next (locals)))),
+  ]) (req)
