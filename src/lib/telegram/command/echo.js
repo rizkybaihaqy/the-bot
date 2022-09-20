9d28c958231ed97341440edf7733db0c43eaa1b4
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-09-20 10:51:28 +0700

    change echo cmd to use getbotcmdarg

--- src/lib/telegram/command/echo.js
+++ src/lib/telegram/command/echo.js
@@ -1,26 +1,11 @@
 import {JSONData, eitherToFuture} from '../../fluture'
 import {S} from '../../sanctuary/instance'
-import {
-  getEntityLength,
-  getEntityOffset,
-  getTextFromRequest,
-} from '../getter'
+import {getBotCommandArgument} from '../getter'
 import {replyTo} from '../request'
 
 export const echo = (req) =>
   S.pipe ([
-    getTextFromRequest,
-    S.map ((txt) =>
-      txt.slice (
-        getEntityOffset (req) + getEntityLength (req),
-      ),
-    ),
-    S.map (S.trim),
-    S.chain (
-      S.ifElse ((txt) => txt === '') ((_) =>
-        S.Left ('Cannot Echo Something That Does Not Exist'),
-      ) (S.Right),
-    ),
+    getBotCommandArgument,
     eitherToFuture,
     S.chain (replyTo (req)),
     S.map (JSONData),
