48459e046f6303f5d26078eb49f8b588bf5080a8
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-10-17 16:34:45 +0700

    add util getter test

---
+++ src/lib/utils/__tests__/getter.test.js
@@ -0,0 +1,23 @@
+import {getArrayElement} from '../getter'
+import {S} from '../../sanctuary'
+
+describe ('getArrayElement', () => {
+  it ('Should get single element from array at index 1', () => {
+    const elm = getArrayElement (0) ([ 1, 2, 3, 4 ])
+    expect (S.show (elm)).toBe ('Just (1)')
+  })
+
+  it ('Should return Nothing if requested index is out of bound', () => {
+    const elm = getArrayElement (5) ([ 1, 2, 3, 4 ])
+    const empty = getArrayElement (1) ([])
+    expect (S.show (elm)).toBe ('Nothing')
+    expect (S.show (empty)).toBe ('Nothing')
+  })
+
+  it ('Should return Nothing if requested index is < 0', () => {
+    const elm = getArrayElement (-1) ([ 1, 2, 3, 4 ])
+    const empty = getArrayElement (-1) ([ 1, 2, 3, 4 ])
+    expect (S.show (elm)).toBe ('Nothing')
+    expect (S.show (empty)).toBe ('Nothing')
+  })
+})
