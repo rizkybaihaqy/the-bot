7b78c38e21639dccc9d534bb8e275db6098fce67
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-01-08 19:07:19 +0700

    add validation to visit repot

--- src/actions/visit/report.js
+++ src/actions/visit/report.js
@@ -1,3 +1,4 @@
+import {capitalCase, snakeCase} from 'change-case'
 import {Next} from 'fluture-express'
 import {
   F,
@@ -10,6 +11,14 @@ import {
   getMessageFromRequest,
   getTextFromMessage,
 } from '../../lib/telegram/getter'
+import {objDiff} from '../../lib/utils/getter'
+import {visitRules} from '../../rules/visit'
+
+// StrMap
+const visitRulesWithoutLocationTelegramId = S.pipe ([
+  S.remove ('location'),
+  S.remove ('telegram_id'),
+]) (visitRules)
 
 // Req -> boolean
 const isVisitReport = S.pipe ([
@@ -19,14 +28,47 @@ const isVisitReport = S.pipe ([
   S.fromRight (false),
 ])
 
+// Message -> Either String StrMap String
+const getVisitDataFromMessage = S.pipe ([
+  getTextFromMessage,
+  S.map (S.lines),
+  S.map (S.map (S.splitOn (':'))),
+  S.map (S.filter ((x) => x.length === 2)),
+  S.map (
+    S.map (([ key, value ]) =>
+      S.Pair (snakeCase (key)) (S.trim (value)),
+    ),
+  ),
+  S.map (S.fromPairs),
+])
+
+// StrMap -> Either String StrMap String
+const validateUserInput = (rules) =>
+  S.ifElse (S.pipe ([ objDiff (rules), (x) => x.length === 0 ])) (
+    S.pipe ([ S.ap (rules), S.sequence (S.Either) ]),
+  ) (
+    S.pipe ([
+      objDiff (rules),
+      S.map (capitalCase),
+      (x) => 'Missing Field ' + x,
+      S.Left,
+    ]),
+  )
+
 export default (locals) =>
   S.ifElse (isVisitReport) (
     S.pipe ([
       getMessageFromRequest,
-      S.chain (getTextFromMessage),
-      S.map ((text) =>
-        text.replace ('#VisitReport', '#VisitSubmit'),
+      S.chain (getVisitDataFromMessage),
+      S.chain (
+        validateUserInput (
+          visitRulesWithoutLocationTelegramId,
+        ),
       ),
+      S.map (S.pairs),
+      S.map (S.map ((x) => S.fst (x) + ':' + S.snd (x))),
+      S.map (S.unlines),
+      S.map ((x) => '#VisitSubmit\n' + x),
       eitherToFuture,
       S.chain (
         locals.sendMessage ({
