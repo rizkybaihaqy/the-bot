f29e26df8be9dfc6566ac0aaee8432fc6255a619
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-01-09 17:47:02 +0700

    add getvisitupdate use case

--- src/use-case/visit.js
+++ src/use-case/visit.js
@@ -1,6 +1,12 @@
-import {findOneSales} from '../data-access/sales'
-import {insertOneToVisits} from '../data-access/visits'
-import {F} from '../lib/fluture'
+import {
+  findOneSalesById,
+  findOneSalesByTelegramId,
+} from '../data-access/sales'
+import {
+  findAllTodayVisits,
+  insertOneToVisits,
+} from '../data-access/visits'
+import {F, maybeToFuture} from '../lib/fluture'
 import {S} from '../lib/sanctuary'
 
 // StrMap String -> Future String String
@@ -11,7 +17,7 @@ export const addVisit = S.pipe ([
     ) (
       S.pipe ([
         S.prop ('telegram_id'),
-        findOneSales,
+        findOneSalesByTelegramId,
         S.chain (
           S.ifElse ((x) => x.rowCount === 0) ((_) =>
             F.reject (
@@ -23,4 +29,23 @@ export const addVisit = S.pipe ([
     ),
   S.map ((x) => ({...x[0], sales_id: x[1]})),
   S.chain ((x) => insertOneToVisits (S.keys (x)) (S.values (x))),
+])
+
+// String -> Future String String
+export const getVisitUpdate = S.pipe ([
+  (salesId) =>
+    F.both (
+      S.pipe ([
+        findOneSalesById,
+        S.map (S.prop ('rows')),
+        S.map (S.head),
+        S.chain (maybeToFuture),
+      ]) (salesId),
+    ) (
+      S.pipe ([
+        findAllTodayVisits,
+        S.map (S.prop ('rowCount')),
+      ]) (new Date (Date.now ()).toISOString ()),
+    ),
+  S.map ((x) => ({user: x[0], todayVisit: x[1]})),
 ])
