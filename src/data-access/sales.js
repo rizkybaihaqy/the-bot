f105127a0bee2f6526e4b2fb176df4470fb3e57a
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-01-09 17:46:13 +0700

    add find sales by id

--- src/data-access/sales.js
+++ src/data-access/sales.js
@@ -3,11 +3,19 @@ import {pgFlQuery} from '../db/instance'
 import Sales from '../models/Sales'
 
 // String -> Future Error pg.Result
-export const findOneSales = (telegramId) =>
+export const findOneSalesByTelegramId = (telegramId) =>
   pgFlQuery ({
     name: 'select one sales by telegram id',
     text: 'SELECT id FROM sales WHERE telegram_id=$1',
     values: [telegramId],
+  })
+
+// String -> Future Error pg.Result
+export const findOneSalesById = (salesId) =>
+  pgFlQuery ({
+    name: 'select one sales by sales id',
+    text: 'SELECT id::text, name::text FROM sales WHERE id=$1',
+    values: [salesId],
   })
 
 // Array Array String -> Future Error pg.Result
