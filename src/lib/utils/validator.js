85c2a29db09ce8b22d397f19733fa94b140ccf4c
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-01-12 00:06:26 +0700

    fix wrong comment

--- src/lib/utils/validator.js
+++ src/lib/utils/validator.js
@@ -2,7 +2,7 @@ import {capitalCase} from 'change-case'
 import {S} from '../sanctuary'
 import {objDiff} from './getter'
 
-// StrMap F StrMap String -> Either String StrMap String
+// StrMap F -> StrMap String -> Either String StrMap String
 export const validate = (rules) =>
   S.ifElse (S.pipe ([ objDiff (rules), (x) => x.length === 0 ])) (
     S.pipe ([ S.ap (rules), S.sequence (S.Either) ]),
