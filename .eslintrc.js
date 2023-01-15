11ba460a88d79b34d6875b78d46ba5a39d5c78e3
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-01-15 21:51:00 +0700

    add jest eslint and lint script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,26 @@
 module.exports = {
   parser: '@babel/eslint-parser',
+  plugins: ['functional', 'jest'],
   env: {
-    es6: true,
-    node: true,
+    'es6': true,
+    'node': true,
+    'jest/globals': true,
   },
-  plugins: ['functional'],
   extends: [
     'plugin:functional/external-recommended',
     'plugin:functional/recommended',
     'plugin:functional/stylistic',
     './node_modules/sanctuary-style/eslint-es6.json',
+  ],
+  overrides: [
+    {
+      files: ['src/**/*.js'],
+      extends: ['plugin:jest/recommended', 'plugin:jest/style'],
+      rules: {
+        "functional/no-expression-statement": ['off'],
+        "functional/functional-parameter": ['off']
+      }
+    },
   ],
   rules: {
     semi: ['error', 'never'],
