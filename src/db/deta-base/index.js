593d5864f2b9f9111d7520d16d3054d3ff7bb03d
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-01-20 10:20:56 +0700

    fix eslint tacit @detabase init

--- src/db/deta-base/index.js
+++ src/db/deta-base/index.js
@@ -2,5 +2,8 @@ import {Deta} from 'deta'
 import {DETA_KEY} from '../../config'
 import {F} from '../../lib/fluture'
 
+const deta = Deta (DETA_KEY)
+
 export const flDetaBase = method =>
-  F.encaseP (Deta (DETA_KEY).Base ('the-bot')[method])
+  // eslint-disable-next-line functional/prefer-tacit
+  F.encaseP (data => deta.Base ('the-bot')[method] (data))
