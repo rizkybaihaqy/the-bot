13f0dd6e6761a65ca499d1169ca212edbd4a7e12
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-01-29 22:33:14 +0700

    remove core js and runtime generator

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,34 +1,12 @@
 #!/usr/bin/env node
 
 /* eslint-disable */
-import 'core-js/stable'
-import http from 'http'
-import 'regenerator-runtime/runtime'
 
 /**
  * Module dependencies.
  */
+import http from 'http'
 import app from '../app'
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-const normalizePort = (val) => {
-  const port = parseInt(val, 10)
-
-  if (Number.isNaN(port)) {
-    // named pipe
-    return val
-  }
-
-  if (port >= 0) {
-    // port number
-    return port
-  }
-
-  return false
-}
 
 /**
  * Get port from environment and store in Express.
@@ -44,27 +22,55 @@ app.set('port', port)
 const server = http.createServer(app)
 
 /**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    // named pipe
+    return val
+  }
+
+  if (port >= 0) {
+    // port number
+    return port
+  }
+
+  return false
+}
+
+/**
  * Event listener for HTTP server "error" event.
  */
 
-const onError = (error) => {
+function onError(error) {
   if (error.syscall !== 'listen') {
     throw error
   }
 
   const bind =
     typeof port === 'string'
-      ? `Pipe ${port}`
-      : `Port ${pipe}`
+      ? 'Pipe ' + port
+      : 'Port ' + port
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(`${bind} requires elevated privileges`)
+      console.error(bind + ' requires elevated privileges')
       process.exit(1)
       break
     case 'EADDRINUSE':
-      console.error(`${bind} is already in use`)
+      console.error(bind + ' is already in use')
       process.exit(1)
       break
     default:
@@ -76,19 +82,11 @@ const onError = (error) => {
  * Event listener for HTTP server "listening" event.
  */
 
-const onListening = () => {
+function onListening() {
   const addr = server.address()
   const bind =
     typeof addr === 'string'
-      ? `pipe ${addr}`
-      : `port ${addr.port}`
-  console.log(`Listening on ${bind}`)
+      ? 'pipe ' + addr
+      : 'port ' + addr.port
+  console.log('Listening on ' + bind)
 }
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port)
-server.on('error', onError)
-server.on('listening', onListening)
