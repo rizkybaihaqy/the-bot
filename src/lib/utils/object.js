010e084318388cfbb4cce1e66a45ecf63386b0d0
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-02-04 23:39:33 +0700

    move objDiff to obj from strmap

--- src/lib/utils/object.js
+++ src/lib/utils/object.js
@@ -1,5 +1,10 @@
 import $ from 'sanctuary-def'
 import {S} from '../sanctuary'
+import {arrayDiff} from './array'
 
 export const get = S.get (S.is ($.Any))
 export const gets = S.gets (S.is ($.Any))
+
+// Object -> Object -> Array String
+export const objDiff = base => comparator =>
+  arrayDiff (S.keys (base)) (S.keys (comparator))
--- src/lib/utils/strmap.js
+++
@@ -1,6 +0,0 @@
-import {S} from '../sanctuary'
-import {arrayDiff} from './array'
-
-// StrMap -> StrMap -> Array String
-export const objDiff = base => comparator =>
-  arrayDiff (S.keys (base)) (S.keys (comparator))
