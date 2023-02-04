a8ff66d275bcdc3cc9520fb9410b1887de5609d9
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-02-04 07:05:15 +0700

    add lift3_

--- src/lib/utils/function.js
+++ src/lib/utils/function.js
@@ -3,6 +3,10 @@ import {S} from '../sanctuary'
 export const lift2_ = fn => fa => fb => data =>
   S.lift2 (fn) (fa (data)) (fb (data))
 
+export const lift3_ =
+  fn => fa => fb => fc => data =>
+    S.lift3 (fn) (fa (data)) (fb (data)) (fc (data))
+
 export const alt_ = fa => fb => data =>
   S.alt (fa (data)) (fb (data))
 
